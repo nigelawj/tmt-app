c58ad4989f30116d998472a19ffe9afd780021ea
Nigel <nigelawj@gmail.com>
2019-09-21 22:16:30 +0800

    210919 2nd Push
    EDIT:
    - Removed some unused packages
    - Edited INFO button in Game page
    
    1st Push:
    Mostly completed
    
    - Added ability to add results to be viewed on a graph, and toggling
    - Added some styling:
    > Implemented slider button for Doctors page
    > Implemented a confirmation dialog for Deleting Result
    - Refactored some code
    
    TO-DO:
    - Implement a verdict into results (To be discussed further)
    - Styling
    - Contact page + Feedback forms + pre-trial notifications

--- client/src/components/layout/GameNav.js
+++ client/src/components/layout/GameNav.js
@@ -1,22 +1,68 @@
-import React, { useContext } from 'react';
-import Popup from 'reactjs-popup';
+import React, { useContext, useState } from 'react';
 import './GameNavLayout.css';
 
 import GameContext from '../../context/game/gameContext';
 import Errors from '../game/Errors';
 
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Slide
+} from '@material-ui/core';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const GameNav = () => {
   const gameContext = useContext(GameContext);
   const { initGame, end } = gameContext;
+
+  const [open, setOpen] = useState(false);
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
 
   return (
     <div className="navbar bg-primary">
       <ul>
         <li>{!end ? <button onClick={initGame}>RESET</button> : null}</li>
       </ul>
-      <Popup trigger={<button>About</button>} position="right center">
-        <div>Popup content here !!</div>
-      </Popup>
+      <button style={{ float: 'right' }} onClick={handleClickOpen}>
+        INFO
+      </button>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {'How to play the game'}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Connect the 25 nodes. The path you trace will be displayed. Timing
+            begins after you click on the first node!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            OK!
+          </Button>
+        </DialogActions>
+      </Dialog>
       <Errors></Errors>
     </div>
   );
